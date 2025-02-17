94918878fd1b6399f29592234101e56661abf163
Yuji Oda <yujioda1703@gmail.com>
2025-02-17 09:18:51 -0300

    Aula sobre estrutura e códigos básicos em Java

---
+++ aula01/Main.java
@@ -0,0 +1,10 @@
+import java.nio.channels.Pipe.SourceChannel;
+
+public class Main 
+{
+        public static void main(String[] args) 
+    {
+        System.out.println("Hello World");
+        System.out.println("Pinto");
+    }
+}

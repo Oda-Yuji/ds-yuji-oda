859e943260ddd5e9a8b23dcfec60a2111ba030f0
Yuji Oda <yujioda1703@gmail.com>
2025-02-24 09:30:07 -0300

    Aula sobre variáveis

---
+++ aula02/Variaveis.java
@@ -0,0 +1,25 @@
+public class Variaveis
+{
+    public static void main(String[] args)
+    {
+        String name = "Alexandre";
+        String rosto = "Autroloptécus";
+        System.out.println(name);
+        System.out.println(rosto);
+
+        int n1 = 22;
+        int n2 = 9;
+        int n3 = 4;
+        System.out.println(n1);
+        System.out.println(n2 + n3);
+
+        float nf1 = 101.1f;
+        System.out.println(nf1);
+
+        boolean nameIgualRosto = true;
+        System.out.println(nameIgualRosto);
+
+        char dDeDeus = 'd';
+        System.out.println(dDeDeus);
+    }
+}

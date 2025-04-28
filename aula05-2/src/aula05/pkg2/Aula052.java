d1d901f221143ee9db3109e713fd8272226f7751
Yuji Oda <yujioda0317@gmail.com>
2025-04-28 08:53:38 -0300

    Introdução ao NetBeans

---
+++ aula05-2/src/aula05/pkg2/Aula052.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula05.pkg2;
+
+import java.util.Scanner;
+/**
+ *
+ * @author CAMARGO
+ */
+public class Aula052 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Valor de A: ");
+        double a = scan.nextDouble();
+        
+        System.out.println("Valor de B: ");
+        double b = scan.nextDouble();
+        
+        double soma = a + b;
+        
+        System.out.println(soma);
+    }
+    
+}
---
+++ aula05-2/src/aula05/pkg2/Exercicio1.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula05.pkg2;
+
+import java.util.Scanner;
+/**
+ *
+ * @author CAMARGO
+ */
+
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+        double gastosJaneiro = 15000;
+            
+        double gastosFevereiro = 23000;
+            
+        double gastosMarco = 17000;
+            
+        double gastosTrimestre = gastosJaneiro + gastosFevereiro + gastosMarco;
+            
+        System.out.println(gastosTrimestre);
+    }
+}

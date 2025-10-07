e2a36376e04f42606375fc3ac93d923922060a5b
Yuji Oda <yujioda1703@gmail.com>
2025-10-07 16:23:48 -0300

    RESt API com CRUD

--- crudapi/src/test/java/com/example/crudapi/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.crudapi.repository;
-
-import com.example.crudapi.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
